d6816e0862249155c6294127eff529a9bfbc66e0
mr.scrpt <mr.scrpt@gmail.com>
2023-12-01 14:21:37 +0200

    implemented: base popup style with border

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Banner } from "@/component/Banner/Banner";
 import { Overlay } from "@/component/Overlay/Overlay";
+import { Popup } from "@/component/Popup/Popup";
 import { Wheel } from "@/component/Wheel/Wheel";
 
 export default function Home() {
@@ -7,7 +8,12 @@ export default function Home() {
     <main className="grid grid-rows-2 grid-cols-1 md:grid-cols-2 md:grid-rows-1 items-center min-h-screen deco-bg-lines">
       <Banner className=" relative z-10" />
       <Wheel className=" relative z-10" />
-      <Overlay />
+      <Overlay>
+        <Popup
+          textBenefit="225 free spins"
+          textAbout="You’ve Got 1 Attempt Left"
+        />
+      </Overlay>
     </main>
   );
 }
--- component/Popup/Popup.tsx
+++ component/Popup/Popup.tsx
@@ -2,8 +2,16 @@ import { FC, HTMLAttributes } from "react";
 
 interface PopupProps extends HTMLAttributes<HTMLDivElement> {
   textBenefit: string;
+  textAbout: string;
 }
 
 export const Popup: FC<PopupProps> = (props) => {
-  return <div>Popup</div>;
+  const { className, textBenefit, textAbout } = props;
+  return (
+    <div className={className}>
+      <div className="flex flex-col deco-gradient-border relative">
+        <div className="uppercase">Congrats!</div>
+      </div>
+    </div>
+  );
 };
